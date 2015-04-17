83ca1af6395c83cf53b0aee2df5126e024b34ad8
John Stevenson <john@jr0cket.co.uk>
2015-04-17 17:38:20 +0100

    A simple explination of lists
    Using a shopping list analogy to explain how lists work in Clojure

---
+++ src/clojure_bridge_exercises/exercises.clj
@@ -0,0 +1,16 @@
+(ns clojure-bridge-exercises.exercises)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Introduction to Programming with Clojure
+
+
+;; Some really basic concepts
+
+;; Everything is a list in Clojure.  List stands for List Processing.  So what is a list and does it work
+
+;; Really simplistic example - a shopping list
+
+(shopping-in-tesco "eggs" "bacon" "cheese" "cerial" "milk")
+
+;; We have a list of things we want to buy.  the first item in the list defines how we are going to get these things
+
