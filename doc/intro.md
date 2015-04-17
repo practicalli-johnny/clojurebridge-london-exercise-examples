f7a6f39fde88bb933e5b041d668ed9d570cab846
John Stevenson <john@jr0cket.co.uk>
2015-04-17 17:34:16 +0100

    Initial project for ClojureBridge London exercises
    A project to work through the exercises at https://github.com/ClojureBridgeLondon/curriculum
    
    Also adding examples around those exercises to help explore the clojure language

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-bridge-exercises
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_bridge_exercises/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-bridge-exercises.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-bridge-exercises.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
